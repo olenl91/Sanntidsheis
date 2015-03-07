dd47087a765f53e6785b0e68ac179a76801895c7
Kjetil Kjeka <kjetibk@stud.ntnu.no>
2015-03-07 16:51:44 +0100

    Major update of c wrappers

--- driver/elev_port.c
+++ driver/elev_port.c
@@ -9,6 +9,8 @@ int write_cmd(byte *buf, int len);
 int read_exact(byte *buf, int len);
 int write_exact(byte *buf, int len);
 
+void poll_button_signals();
+void poll_floor_sensors();
 
 int read_cmd(byte *buf)
 {
@@ -62,54 +64,124 @@ int write_exact(byte *buf, int len)
 
 int main() 
 {
-  int res;
   byte buf[100];
 
-  while (read_cmd(buf) > 0) {
 
-    switch(buf[0]){
-    case(1):
-      elev_init(buf[1]);
-      res = 0;
-      break;
-    case(2):
-      elev_set_motor_direction(buf[1]);
-      res = buf[1];
-      break;
-    case(3):
-      elev_set_door_open_lamp(buf[1]);
-      res = 0;
-      break;
-    case(4):
-      res = elev_get_obstruction_signal();
-      break;
-    case(5):
-      res = elev_get_stop_signal();
-      break;
-    case(6):
-      elev_set_stop_lamp(buf[1]);
-      res = 0;
-      break;
-    case(7):
-      res = elev_get_floor_sensor_signal();
-      break;
-    case(8):
-      elev_set_floor_indicator(buf[1]);
-      res = 0;
-      break;
-    case(9):
-      res = elev_get_button_signal(buf[1], buf[2]);
-      break;
-    case(10):
-      elev_set_button_lamp(buf[1], buf[2], buf[3]);
-      res = 0;
-      break;
+  while (1) {
 
-    }
+      if(read_cmd(buf) > 0){
 
-    buf[0] = res;
-    write_cmd(buf, 1);
+	  switch(buf[0]){
+	  case(1):
+	      elev_init(buf[1]);
+	      break;
+	  case(2):
+	      elev_set_motor_direction(buf[1]);
+	      break;
+	  case(3):
+	      elev_set_door_open_lamp(buf[1]);
+	      break;
+	  case(4):
+	      buf[0] = elev_get_obstruction_signal();
+	      write_cmd(buf, 1);
+	      break;
+	  case(5):
+	      buf[0] = elev_get_stop_signal();
+	      write_cmd(buf, 1);
+	      break;
+	  case(6):
+	      elev_set_stop_lamp(buf[1]);
+	      break;
+	  case(7):
+	      buf[0] = elev_get_floor_sensor_signal();
+	      write_cmd(buf, 1);
+	      break;
+	  case(8):
+	      elev_set_floor_indicator(buf[1]);
+	      break;
+	      /* case(9): */
+	      /* 	  res = elev_get_button_signal(buf[1], buf[2]); */
+	      /* 	  break; */
+	  case(10):
+	      elev_set_button_lamp(buf[1], buf[2], buf[3]);
+	      break;	  
+	  case(11):
+	      poll_button_signals();
+	      break;
+	  case(12):
+	      poll_floor_sensors();
+
+	  }
+      }
+      
+
   }
 
   return 0;
+}
+
+
+void poll_floor_sensors()
+{
+    byte buf[100];
+
+    static int previousFloor;
+
+    int newFloor = elev_get_floor_sensor_signal();
+
+    buf[0] = 12;
+
+    if(newFloor != -1 && newFloor != previousFloor){
+	buf[1] = newFloor;
+	write_cmd(buf, 2);
+    }
+    
+    previousFloor = newFloor;
+
+}
+
+
+void poll_button_signals()
+{
+    static int previousUp[N_FLOORS];
+    static int previousDown[N_FLOORS];
+    static int previousCommand[N_FLOORS];
+    
+    byte buf[100];
+
+
+
+    for(int i = 0; i < N_FLOORS; i++) // poller masse, fix
+    {
+	buf[0] = 11;
+
+	if(i != N_FLOORS-1){
+	    int up = elev_get_button_signal(BUTTON_CALL_UP, i);
+	    if(up && !previousUp[i]){
+		buf[1] = 0;
+		buf[2] = i;
+		write_cmd(buf, 3);
+	    }
+	    previousUp[i] = up;
+	}
+	
+	if(i != 0){
+	    int down = elev_get_button_signal(BUTTON_CALL_DOWN, i);
+	    if(down && !previousDown[i]){
+		buf[1] = 1;
+		buf[2] = i;
+		write_cmd(buf, 3);
+	    }
+	    previousDown[i] = down;
+	}
+	
+	int command = elev_get_button_signal(BUTTON_COMMAND, i);
+	if(command && previousCommand[i]){
+	    buf[1] = 2;
+	    buf[2] = i;
+	    write_cmd(buf, 3);
+	}
+	previousCommand[i] = command;
+      }
+   
 }
--- elev_driver.erl
+++ elev_driver.erl
@@ -1,55 +1,66 @@
--module(elev).
--export([start/0, stop/0, init/1]).
--export([elev_init/1, elev_set_motor_direction/1, elev_set_door_open_lamp/1, elev_get_obstruction_signal/0, elev_get_stop_signal/0, elev_set_stop_lamp/1, elev_get_floor_sensor_signal/0, elev_set_floor_indicator/1, elev_get_button_signal/2, elev_set_button_lamp/3]).
+-module(elev_driver).
+-export([start/0, stop/0, init_port/1]).
+-export([init/1, set_motor_direction/1, set_door_open_lamp/1, set_stop_lamp/1, set_floor_indicator/1, set_button_lamp/3]).
+
 
 start() ->
-    spawn(?MODULE, init, ["driver/elev_port"]).
+    register(caller, self()),
+    spawn(?MODULE, init_port, ["driver/elev_port"]),
+    timer:sleep(10),
+    init(simulator),
+    spawn(fun() -> poll_everything() end).
+		  
 stop() ->
-    complex ! stop.
+    driver ! stop.
 
-elev_init(simulator) ->
+
+poll_everything() ->
+    poll_order_buttons(),
+    poll_floor_sensors(),
+    timer:sleep(10),
+    poll_everything().
+
+
+init(simulator) ->
     call_port({elev_init, 1});
-elev_init(elevator) ->
+init(elevator) ->
     call_port({elev_init, 2}).
 
-elev_set_motor_direction(Dirn) ->
-    call_port({elev_set_motor_direction, Dirn}).
+set_motor_direction(up) ->
+    call_port({elev_set_motor_direction, 1});
+set_motor_direction(down) ->
+    call_port({elev_set_motor_direction, 2});
+set_motor_direction(stop) ->
+    call_port({elev_set_motor_direction, 0}).
 
-elev_set_door_open_lamp(Value) ->
-    call_port({elev_set_door_open_lamp, Value}).
+set_door_open_lamp(on) ->
+    call_port({elev_set_door_open_lamp, 1});
+set_door_open_lamp(off) ->
+    call_port({elev_set_door_open_lamp, 0}).
 
-elev_get_obstruction_signal() ->
-    call_port({elev_get_obstruction_signal}).
+set_stop_lamp(on) ->
+    call_port({elev_set_stop_lamp, 1});
+set_stop_lamp(off) ->
+    call_port({elev_set_stop_lamp, 0}).
 
-elev_get_stop_signal() ->
-    call_port({elev_get_stop_signal}).
-
-elev_set_stop_lamp(Value) ->
-    call_port({elev_set_stop_lamp, Value}).
-
-elev_get_floor_sensor_signal() ->
-    call_port({elev_get_floor_sensor_signal}).
-
-elev_set_floor_indicator(Floor) ->
+set_floor_indicator(Floor) -> % maybe add some special case when Floor > 3, or crash!!!!!?
     call_port({elev_set_floor_indicator, Floor}).
 
-elev_get_button_signal(Button, Floor) ->
-    call_port({elev_get_button_signal, Button, Floor}).
-
-elev_set_button_lamp(Button, Floor, Value) ->
+set_button_lamp(Button, Floor, Value) ->
     call_port({elev_set_button_lamp, Button, Floor, Value}).
 
+poll_order_buttons() ->
+    call_port({poll_order_buttons}).
+
+poll_floor_sensors() ->
+    call_port({poll_floor_sensors}).
 
 
 call_port(Msg) ->
-	complex ! {call, self(), Msg},
-    	receive
-		{complex, Result} ->
-	    	Result
-    	end.
+    driver ! {call, self(), Msg}.
 
-init(ExtPrg) ->
-    register(complex, self()),
+init_port(ExtPrg) ->
+    register(driver, self()),
     process_flag(trap_exit, true),
     Port = open_port({spawn, ExtPrg}, [{packet, 2}]),
     loop(Port).
@@ -58,10 +69,18 @@ loop(Port) ->
     receive
 	{call, Caller, Msg} ->
 	    Port ! {self(), {command, encode(Msg)}},
-	    receive
-		{Port, {data, Data}} ->
-		    Caller ! {complex, decode(Data)}
-	    end,
+	    loop(Port); 
+	{Port, {data, [11, 0, Floor]}} ->
+	    caller ! {new_order, up, Floor},
+	    loop(Port);
+	{Port, {data, [11, 1, Floor]}} ->
+	    caller ! {new_order, down, Floor},
+	    loop(Port);
+	{Port, {data, [11, 2, Floor]}} ->
+	    caller ! {new_order, command, Floor},
+	    loop(Port);
+	{Port, {data, [12, Floor]}} ->
+	    caller ! {floor_reached, Floor},
 	    loop(Port);
 	stop ->
 	    Port ! {self(), close},
@@ -82,8 +101,8 @@ encode({elev_set_stop_lamp, Value}) -> [6, Value];
 encode({elev_get_floor_sensor_signal}) -> [7];
 encode({elev_set_floor_indicator, Floor}) -> [8, Floor];
 encode({elev_get_button_signal, Button, Floor}) -> [9, Button, Floor];
-encode({elev_set_button_lamp, Button, Floor, Value}) -> [10, Button, Floor, Value].
-
-
-decode([Int]) -> Int.
+encode({elev_set_button_lamp, Button, Floor, Value}) -> [10, Button, Floor, Value];
+encode({poll_order_buttons}) -> [11];
+encode({poll_floor_sensors}) -> [12].
+    
 
