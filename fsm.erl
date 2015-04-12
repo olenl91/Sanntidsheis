1616eb4a903cb24415cad3cc5a89cec8f20fcc30
Kjetil Kjeka <kjetilkjeka@gmail.com>
2015-04-12 20:18:58 +0200

    major cleanup in fsm module

--- fsm.erl
+++ fsm.erl
@@ -1,65 +1,70 @@
 -module(fsm).
--export([start/0, go_direction/1, event_floor_reached/1]).
+-compile(export_all).
+%-export([start/0, go_direction/1, event_floor_reached/1]).
 
-start() ->
-    register(fsm, spawn(fun() -> init() end)).
-%    spawn(?MODULE, init).
+%% Module Interface
+%%%%%%%%%%%%%%%%%%%%
+
+go_direction(Pid, up) ->
+    Pid ! up;
+go_direction(Pid, down) ->
+    Pid ! down;
+go_direction(Pid, open) ->
+    Pid ! open.
+
+event_floor_reached(Pid) ->
+    Pid ! floor_reached.
+
+
+%% Process Functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+start(Listener) ->
+    spawn(fun() -> init(Listener) end).
     
 
-go_direction(up) ->
-    fsm ! {go, up};
-go_direction(down) ->
-    fsm ! {go, down};
-go_direction(open) ->
-    fsm ! {go, open}.
-
-event_floor_reached(Floor) ->
-    fsm ! {floor_reached, Floor}.
+%% States
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-
-init() ->
-    event_manager ! {set_motor_direction, up},
+init(Listener) ->
+    Listener ! {motor, up},
     receive
-	{floor_reached, Floor} ->
-	    idle(Floor)
+	floor_reached ->
+	    idle(Listener)
     end.
 
-driving_up(LastFloor) ->
-    event_manager ! {set_motor_direction, up},
+driving_up(Listener) ->
+    Listener ! {motor, up},
     receive
-	{floor_reached, Floor} ->
-	    idle(Floor)
+	floor_reached ->
+	    idle(Listener)
     end.
 
 
-driving_down(LastFloor) ->
-    event_manager ! {set_motor_direction, down},
+driving_down(Listener) ->
+    Listener ! {motor, down},
     receive
-        {floor_reached, Floor} ->
-	    idle(Floor)
-						%lol
+        floor_reached ->
+	    idle(Listener)
     end.
 
 
-idle(ThisFloor) ->
-						% request order from queue, every now and then,
-    event_manager ! {set_motor_direction, stop},
-    event_manager ! {fsm_info, stopped, ThisFloor},
-						% set floor indicato
+idle(Listener) ->
+    Listener ! {motor, stop},
     receive
-	{go, up} ->
-	    driving_up(ThisFloor);
-	{go, down} ->
-	    driving_down(ThisFloor);
-	{go, open} ->
-	    open_doors(ThisFloor)
-		
+	up ->
+	    driving_up(Listener);
+	down ->
+	    driving_down(Listener);
+	open ->
+	    open_doors(Listener)		
     end.
 
-open_doors(ThisFloor) ->
-    elev:elev_set_door_open_lamp(1),
+open_doors(Listener) ->
+    Listener ! {doors, open},
     timer:sleep(3000),
-    elev:elev_set_door_open_lamp(0),
-    idle(ThisFloor).
+    Listener ! {doors, close},
+    idle(Listener).
 
