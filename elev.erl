e00e9d8000dc7b87d61020cba25f6bdf17cc0e6c
Kjetil Kjeka <kjetilkjeka@gmail.com>
2015-04-14 13:56:56 +0200

    Major rewrite of elev.erl to accomodate for rewrites of other modules. Elev/managers handle most of the communication between modules as is. Need to implement a bit more around deleting orders and event sending.

--- elev.erl
+++ elev.erl
@@ -2,24 +2,33 @@
 -export([start/0]).
 
 start() ->
-    DriverManagerPID = spawn(fun() -> driver_manager() end),
+    DriverManagerPID = spawn(fun() -> driver_manager_init() end),
     FsmManagerPid = spawn(fun() -> fsm_manager_init() end),
 
     elev_driver:start(DriverManagerPID),
     FsmPID = fsm:start(FsmManagerPid),
     register(fsm, FsmPID),
 
-    QueuePID = queue:start().
+    QueuePID = queue:start(),
+    register(queue, QueuePID).
+
+
 
 fsm_manager_init() -> % dirty hack, plz fix
     timer:sleep(100), % wait for driver initalization
+    queue:floor_reached(queue, 0), % dumb hack
     fsm_manager().
 fsm_manager() ->
     receive
+	{direction, request, Caller} ->
+	    Direction = queue:get_next_direction(queue),
+	    Caller ! {direction, response, Direction};
 	{motor, up} ->
-	    elev_driver:set_motor_direction(up);
+	    elev_driver:set_motor_direction(up),
+	    queue:floor_left(queue, up);
 	{motor, down} ->
-	    elev_driver:set_motor_direction(down);
+	    elev_driver:set_motor_direction(down),
+	    queue:floor_left(queue, down);
 	{motor, stop} ->
 	    elev_driver:set_motor_direction(stop);
 	{doors, open} ->
@@ -29,11 +38,15 @@ fsm_manager() ->
     end,
     fsm_manager().
 
+driver_manager_init() -> % more dirty tricks
+    timer:sleep(100),
+    driver_manager().
 driver_manager() ->
     receive
-	{new_order, Direcetion, Floor} ->
-	    lol;
+	{new_order, Direction, Floor} ->
+	    queue:add(queue, Floor, Direction);
 	{floor_reached, Floor} ->
-	    fsm:event_floor_reached(fsm)
+	    fsm:event_floor_reached(fsm),
+	    queue:floor_reached(queue, Floor)
     end,
     driver_manager().
