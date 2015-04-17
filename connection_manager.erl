2bbaface26c1b3b41520790e506b69496a4e7130
Kjetil Kjeka <kjetilkjeka@gmail.com>
2015-04-17 20:39:31 +0200

    If broadcast_loop and listen_for_connections is running on different nodes they will now connect each other to the node cluster

--- connection_manager.erl
+++ connection_manager.erl
@@ -3,14 +3,21 @@
 
 -define(SEND_PORT, 5678).
 -define(RECV_PORT, 5677).
--define(COOKIE, "erlang"). 
+%-define(COOKIE, "erlang"). 
 -define(SEEK_PERIOD, 5000).
 
 listen_for_connections() ->
     {ok, RecvSocket} = gen_udp:open(?RECV_PORT, [list, {active,false}]),
-    {ok, {Adress, ?SEND_PORT, ?COOKIE}} = gen_udp:recv(RecvSocket, 0), % kan kresje dersom Cookie er feil
-    gen_udp:close(RecvSocket),
-    Adress.
+    {ok, {_Adress, ?SEND_PORT, NodeName}} = gen_udp:recv(RecvSocket, 0), % kan kresje dersom Cookie er feil
+    gen_udp:close(RecvSocket), % probably not smart to open and close connection all the time
+    Node = list_to_atom(NodeName),
+    case is_in_cluster(Node) of
+	true ->
+	    listen_for_connections();
+	false ->
+	    connect_to_node(Node),
+	    listen_for_connections()
+    end.
 
 
 is_in_cluster(Node) ->
@@ -25,7 +32,7 @@ broadcast_loop() ->
     {ok, Socket} = gen_udp:open(?SEND_PORT, [list, {active,true}, {broadcast, true}]),
     broadcast_loop(Socket).
 broadcast_loop(SendSocket) ->
-    ok = gen_udp:send(SendSocket, {255,255,255,255}, ?RECV_PORT, ?COOKIE),
+    ok = gen_udp:send(SendSocket, {255,255,255,255}, ?RECV_PORT, atom_to_list(node())),
     timer:sleep(?SEEK_PERIOD),
     broadcast_loop(SendSocket).
 
