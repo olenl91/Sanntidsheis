d69261fc3f5fdd8f6ab2d16af5cf4ccf0df8176e
olenl91 <ONLyngstadaas@gmail.com>
2015-04-18 13:33:55 +0200

    Working mnesia sync v1.0

--- order_db.erl
+++ order_db.erl
@@ -1,11 +1,11 @@
 -module(order_db).
 -compile(export_all).
-
+-include_lib ("mnesia/src/mnesia.hrl"). %% Include to get the mnesia types, used in create table
 -record(order, {floor, direction}). % might need something about origin to handle internal orders
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% DB interface
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 install(Nodes) ->
     mnesia:create_schema(Nodes),
     rpc:multicall(Nodes, application, start, [mnesia]),
@@ -15,7 +15,6 @@ install(Nodes) ->
 				 {ram_copies, Nodes},
 				 {type, bag}
 				]).
-
 
 add_order(Floor, Direction) ->
     case is_order(Floor, Direction) of
@@ -40,13 +39,41 @@ is_order(Floor, Direction) ->
     OrderList = get_order_list(),
     lists:member(#order{floor = Floor, direction = Direction}, OrderList).
 
+add_new_node_to_cluster(Node) ->
+	net_adm:ping(Node),
+    	rpc:call(Node, application, start, [mnesia]),
+	mnesia:change_config(extra_db_nodes, [Node]),
+	mnesia:change_table_copy_type(schema, Node, ram_copies),
+	mnesia:add_table_copy(orders, Node, ram_copies).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% functions used to reconnect a node after a netsplit
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+sync_mnesia() ->
+	rpc:multicall(mnesia, 'dump_to_textfile', ['BackupMnesia']),
+	reinit_mnesia_cluster(),
+	rpc:multicall(mnesia, 'load_textfile', ['BackupMnesia']).
+	
+reinit_mnesia_cluster() -> 
+	rpc:multicall(mnesia, stop, []),
+	mnesia:delete_schema(lists:append([[node()], nodes()])),
+	install(lists:append([[node()], nodes()])).
+
+%%%%%%%%%%%%%%%%%%%%%%
 %% helper functions
-%%%%%%%%%%%%%%%%%%%    
+%%%%%%%%%%%%%%%%%%%%%%
 
 get_order_list() ->
     GetAllOrdersTransaction = fun() ->
 				      mnesia:match_object(orders, #order{floor='_', direction='_'}, read)
 			      end,
     mnesia:activity(transaction, GetAllOrdersTransaction).
+
+
+
+
+
+
+
+
