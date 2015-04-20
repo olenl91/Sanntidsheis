9dee1ce2cff37220aa23144c0c1643cf4a74fed6
olenl91 <ONLyngstadaas@gmail.com>
2015-04-20 19:12:07 +0200

    Started to replace rpc for ets with new functions
    Still work in progress

--- order_db.erl
+++ order_db.erl
@@ -8,7 +8,25 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 install() ->
-    rpc:multicall(order_db, create_table, []).
+    do_install(lists:append([[node()],nodes()])).
+
+do_install([]) -> ok;
+do_install([Node|NextNodes]) ->
+    case Node == node() of
+        true ->
+            create_table();
+        false ->
+            %spawn process on Node
+            send_table_order(Node),
+            %receive on Node..
+            %and create_table on Node
+        end,
+    do_install(NextNodes).
+
+send_table_order(Node) ->
+    {create_table, Node} ! {Node, do_install}.
+
+%%%% Do similar to replace rpc:multicall for add_order and remove_order
 
 add_order(Floor, Direction) ->
     rpc:multicall(order_db, insert_order, [Floor, Direction]).
@@ -21,8 +39,7 @@ remove_order(Floor, Direction) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 sync_ets() ->
-    NodeList = lists:append([[node()],nodes()]),
-    sync(NodeList).
+    sync(lists:append([[node()],nodes()])).
 
 sync([]) -> ok;
 sync([NodeToSync|NextNodes]) ->
@@ -32,11 +49,11 @@ sync([NodeToSync|NextNodes]) ->
 do_sync([]) -> ok;
 do_sync(NodeToSync) ->
     register(sync, self()),
-    case NodeToSync == node() of   %%maybe a rpc will work instead
+    case NodeToSync == node() of
         true ->
             OrderList = ets:tab2list(orders);
         false ->
-            rpc:call(NodeToSync, order_db, send_Data,[node()]),
+            rpc:call(NodeToSync, order_db, send_order_data,[node()]),
             receive
                     {NodeToSync, OrderList} ->
                         ok
@@ -51,7 +68,7 @@ sync_orders([OrderToSync|NextOrders]) ->
 do_sync_orders(#order{floor = Floor, direction = Direction}) ->
     add_order(Floor, Direction).
 
-send_Data(ToNode) ->
+send_order_data(ToNode) ->
     {sync, ToNode} ! {[node()], ets:tab2list(orders)}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -59,7 +76,7 @@ send_Data(ToNode) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 create_table() ->
-    ets:new(orders, [named_table, {keypos, #order.floor}, bag]).
+    ets:new(orders, [public, named_table, {keypos, #order.floor}, bag]).
 
 insert_order(Floor, Direction) ->
     case is_order(Floor, Direction) of
